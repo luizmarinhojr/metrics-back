8f6981d2c225cc84ae828a1071846ea6514e2440
Luiz Carlos <luizcmoficial@gmail.com>
2025-05-20 10:40:26 -0300

    front-end added

---
+++ back-end/config/config_devel.go
@@ -0,0 +1,9 @@
+package config
+
+const (
+	PSQL_HOST_DEV   = "172.17.0.2"
+	PSQL_PORT_DEV   = "5432"
+	PSQL_USER_DEV   = "postgres"
+	PSQL_PASS_DEV   = "myrandompassword"
+	PSQL_DBNAME_DEV = "representative"
+)
