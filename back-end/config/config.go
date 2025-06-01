2b7d4282f9da7e39d21f33a0f72a2b9afbaae575
Luiz Carlos <luizcmoficial@gmail.com>
2025-06-01 13:03:20 -0300

    rewrite some things

--- back-end/config/config.go
+++ back-end/config/config.go
@@ -6,5 +6,4 @@ var (
 	DB_CONN        = os.Getenv("DB_CONN")
 	DOMAIN         = os.Getenv("DOMAIN_ORIGIN")
 	JWT_SECRET_KEY = os.Getenv("JWT_SECRET_KEY")
-	OWN_DOMAIN     = os.Getenv("OWN_DOMAIN")
 )
