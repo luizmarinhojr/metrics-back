8fb9b741b9d6bba183cea12899ef2573cf9f9ca6
Luiz Carlos <luizcmoficial@gmail.com>
2025-06-01 14:32:49 -0300

    change env path name

--- back-end/config/config.go
+++ back-end/config/config.go
@@ -6,5 +6,5 @@ var (
 	DB_CONN        = os.Getenv("DB_CONN")
 	DOMAIN         = os.Getenv("DOMAIN_ORIGIN")
 	JWT_SECRET_KEY = os.Getenv("JWT_SECRET_KEY")
-	PATH           = os.Getenv("PATH")
+	PATH           = os.Getenv("PATH_COOKIE")
 )
